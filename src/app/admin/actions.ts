aac622f2d50b2b30894ffb5009c99ed4f3e19974
Khasan Otazhonov <otajonovxasan350@gmail.com>
2025-08-20 22:20:39 +0500

    navigation added

---
+++ src/app/admin/actions.ts
@@ -0,0 +1,53 @@
+'use server';
+
+import { auth, clerkClient } from "@clerk/nextjs/server";
+import { Roles } from "../../../types/globals";
+import { revalidatePath } from "next/cache";
+
+export async function setRole(formData: FormData) {
+    const { sessionClaims } = await auth();
+
+    if (sessionClaims?.metadata?.role !== "admin") {
+        throw new Error("Not authorized");
+    }
+
+    const id = formData.get("id") as string;
+    const role = formData.get("role") as Roles;
+    console.log(role);
+
+    try {
+        const clerk = await clerkClient(); // ✅ get the actual client
+
+        await clerk.users.updateUser(id, {
+            publicMetadata: { role },
+        });
+
+        revalidatePath("/admin");
+    } catch (err) {
+        console.error("Error setting role:", err);
+        throw new Error("Failed to set role");
+    }
+}
+
+export async function removeRole(formData: FormData) {
+    const { sessionClaims } = await auth();
+
+    if (sessionClaims?.metadata?.role !== "admin") {
+        throw new Error("Not authorized");
+    }
+
+    const id = formData.get("id") as string;
+
+    try {
+        const clerk = await clerkClient(); // ✅ same fix here
+
+        await clerk.users.updateUser(id, {
+            publicMetadata: { role: null },
+        });
+
+        revalidatePath("/admin");
+    } catch (err) {
+        console.error("Error removing role:", err);
+        throw new Error("Failed to remove role");
+    }
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Navigation } from "@/components/navigation";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,12 +26,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <ClerkProvider>
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Navigation />
+            {children}
+          </ThemeProvider>
       </body>
     </html>
+    </ClerkProvider>
   );
 }
