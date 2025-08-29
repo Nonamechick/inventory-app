abc037aad1d82aa4b03fb79d88c4a42a28c12387
Khasan Otazhonov <otajonovxasan350@gmail.com>
2025-08-29 16:37:37 +0500

    vercel anal.. fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,6 +55,7 @@ export default function RootLayout({
                 {children}
               </div>
             </main>
+            <Analytics />
           </SidebarProvider>
           </ThemeProvider>
       </body>
